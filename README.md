50b3f093ffc384160006d6d72db10564cb8559c1
ThessaM <thessalonica.maria@gmail.com>
2026-02-27 21:42:12 +0700

    Create Flutter Project

---
+++ README.md
@@ -0,0 +1,3 @@
+# main_fltr_lnt_a
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/main_fltr_lnt_a/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.main_fltr_lnt_a
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()

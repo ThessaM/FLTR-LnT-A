0964935c1fbd2990800e4e55a97f3c88febe55eb
ThessaM <thessalonica.maria@gmail.com>
2026-02-28 18:38:28 +0700

    Session 8 Assets and Styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 // import 'package:video_player/video_player.dart';
 
 void main() {
@@ -65,14 +66,31 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late VideoPlayerController videoAssetController;
+  late VideoPlayerController videoNetworkController;
 
   @override
   void initState() {
     super.initState();
+    videoAssetController =
+        VideoPlayerController.asset("assets/videos/video.mp4")
+          ..initialize().then((_) {
+            setState(() {});
+          });
+    videoNetworkController =
+        VideoPlayerController.networkUrl(
+            Uri.parse(
+              "https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4",
+            ),
+          )
+          ..initialize().then((_) {
+            setState(() {});
+          });
   }
 
   @override
   void dispose() {
+    videoAssetController.dispose();
     super.dispose();
   }
 
@@ -83,7 +101,47 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           mainAxisAlignment: .center,
           children: [
-            
+            Image.asset(
+              "assets/images/image.jpg",
+              width: 100,
+              height: 100,
+              fit: .cover,
+            ),
+            SizedBox(height: 20),
+            Image.network(
+              "https://www.vulcantermite.com/wp-content/uploads/2024/01/butterflyvmoth.jpg",
+              width: 100,
+              height: 100,
+              fit: .cover,
+            ),
+            SizedBox(height: 20),
+            GestureDetector(
+              onTap: () {
+                videoAssetController.play();
+              },
+              child: Container(
+                height: 100,
+                width: 100,
+                child: VideoPlayer(videoAssetController),
+              ),
+            ),
+
+            SizedBox(height: 20),
+
+            videoNetworkController.value.isInitialized
+                ? GestureDetector(
+                    onTap: () {
+                      videoNetworkController.value.isPlaying
+                          ? videoNetworkController.pause()
+                          : videoNetworkController.play();
+                    },
+                    child: Container(
+                      height: 100,
+                      width: 100,
+                      child: VideoPlayer(videoNetworkController),
+                    ),
+                  )
+                : CircularProgressIndicator(),
           ],
         ),
       ),
@@ -105,23 +163,57 @@ class _ImageStylingPageState extends State<ImageStylingPage> {
   final styles = ["Circle", "Color Filter", "Rounded", "Border", "Shadow"];
 
   Widget buildStyledImage(String style) {
-    const path = 'assets/image/image.jpg';
+    const path = 'assets/images/image.jpg';
 
     switch (style) {
       case "Circle":
-        return Container();
+        return ClipOval(
+          child: Image.asset(path, width: 150, height: 150, fit: .cover),
+        );
 
       case "Color Filter":
-        return Container();
+        return ClipRect(
+          child: ColorFiltered(
+            colorFilter: .mode(Colors.black, .color),
+            child: Image.asset(path, width: 150, height: 150, fit: .cover),
+          ),
+        );
 
       case "Rounded":
-        return Container();
+        return ClipRRect(
+          borderRadius: .circular(16),
+          child: Image.asset(path, width: 150, height: 150, fit: .cover),
+        );
 
       case "Border":
-        return Container();
+        return Container(
+          height: 150,
+          width: 150,
+          decoration: BoxDecoration(
+            border: .all(color: Colors.purple, width: 5),
+            image: DecorationImage(
+              image: AssetImage(path),
+              fit: .cover,
+              colorFilter: .mode(Colors.amber, .color),
+            ),
+          ),
+        );
 
       case "Shadow":
-        return Container();
+        return Container(
+          height: 150,
+          width: 150,
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.deepOrange,
+                blurRadius: 10,
+                spreadRadius: 10,
+              ),
+            ],
+            image: DecorationImage(image: AssetImage(path), fit: .cover),
+          ),
+        );
 
       default:
         return SizedBox();
@@ -163,7 +255,7 @@ class VideoStylingPage extends StatefulWidget {
 }
 
 class _VideoStylingPageState extends State<VideoStylingPage> {
-  // late VideoPlayerController controller;
+  late VideoPlayerController controller;
   String selectedStyle = "Circle";
 
   final styles = ["Circle", "Color Filter", "Rounded", "Border", "Shadow"];
@@ -171,24 +263,24 @@ class _VideoStylingPageState extends State<VideoStylingPage> {
   @override
   void initState() {
     super.initState();
-    // controller = VideoPlayerController.asset('assets/videos/video.mp4')
-    //   ..initialize().then((_) {
-    //     setState(() {});
-    //     controller.setLooping(true);
-    //     controller.play();
-    //   });
+    controller = VideoPlayerController.asset('assets/videos/video.mp4')
+      ..initialize().then((_) {
+        setState(() {});
+        controller.setLooping(true);
+        controller.play();
+      });
   }
 
   @override
   void dispose() {
-    // controller.dispose();
+    controller.dispose();
     super.dispose();
   }
 
   Widget buildStyledVideo(String style) {
-    // if (!controller.value.isInitialized) {
-    //   return CircularProgressIndicator();
-    // }
+    if (!controller.value.isInitialized) {
+      return CircularProgressIndicator();
+    }
 
     var video = SizedBox(
       width: 120,
@@ -198,26 +290,60 @@ class _VideoStylingPageState extends State<VideoStylingPage> {
         child: SizedBox(
           height: 150,
           width: 150,
-          // child: VideoPlayer(controller),
+          child: VideoPlayer(controller),
         ),
       ),
     );
 
     switch (style) {
       case "Circle":
-        return Container();
+        return Container(
+          width: 150,
+          height: 150,
+          child: ClipOval(child: VideoPlayer(controller)),
+        );
 
       case "Color Filter":
-        return Container();
+        return Container(
+          height: 150,
+          width: 150,
+          child: ClipRect(
+            child: ColorFiltered(
+              colorFilter: .mode(Colors.tealAccent, .darken),
+              child: VideoPlayer(controller),
+            ),
+          ),
+        );
 
       case "Rounded":
-        return Container();
+        return Container(
+          height: 150,
+          width: 150,
+          child: ClipRRect(
+            borderRadius: .circular(16),
+            child: VideoPlayer(controller),
+          ),
+        );
 
       case "Border":
-        return Container();
+        return Container(
+          height: 150,
+          width: 150,
+          decoration: BoxDecoration(
+            border: .all(color: Colors.lime, width: 5)
+          ),
+          child: VideoPlayer(controller),
+        );
 
       case "Shadow":
-        return Container();
+        return Container(
+          height: 150,
+          width: 150,
+          decoration: BoxDecoration(
+            boxShadow: [BoxShadow(color: Colors.red, blurRadius: 10, spreadRadius: 5)]
+          ),
+          child: VideoPlayer(controller),
+        );
 
       default:
         return video;
